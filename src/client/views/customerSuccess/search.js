350ed14c0b944128fc18c2b4b74478ef899aa8ec
longhao <longtask@gmail.com>
2017-06-30 23:07:12 +0800

    search features

---
+++ src/client/views/customerSuccess/search.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lwc on 2017/6/29.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ChatSearch from '../../components/chatSearch';
+
+
+ReactDOM.render(
+    <ChatSearch />
+, document.getElementById('UUCHAT-Search'));
+
