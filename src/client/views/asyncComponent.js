83eca099d0fd6b0e00da010d0b817e10ffd55af8
longhao <longtask@gmail.com>
2017-07-06 21:04:04 +0800

    add new file

---
+++ src/client/views/asyncComponent.js
@@ -0,0 +1,35 @@
+/**
+ * Created by lwc on 2017/7/6.
+ */
+import React from 'react';
+
+export default (loader, collection) => (
+    class AsyncComponent extends React.Component {
+        constructor(props) {
+            super(props);
+
+            this.Component = null;
+            this.state = { Component: AsyncComponent.Component };
+        }
+
+        componentWillMount() {
+            if (!this.state.Component) {
+                loader().then((Component) => {
+                    AsyncComponent.Component = Component;
+
+                    this.setState({ Component });
+                });
+            }
+        }
+
+        render() {
+            if (this.state.Component) {
+                return (
+                    <this.state.Component { ...this.props } { ...collection } />
+            )
+            }
+
+            return null;
+        }
+    }
+);
