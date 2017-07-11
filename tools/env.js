a3baba6ea6ac64ce8a0334524a6a202f31ba29ba
longhao <longtask@gmail.com>
2017-07-11 21:16:20 +0800

    auto CDN

--- tools/env.js
+++ tools/env.js
@@ -20,7 +20,11 @@ function getClientEnvironment(publicUrl) {
             // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
             // This should only be used as an escape hatch. Normally you would put
             // images into the `src` and `import` them in code to get their paths.
-            'PUBLIC_URL': publicUrl
+            'PUBLIC_URL': publicUrl,
+            'reactMinJS': "<%- reactMinJS %>",
+            'reactDomMinJS': "<%- reactDomMinJS %>",
+            'socketIO': "<%- socketIO %>",
+            'momentMinJS': "<%- momentMinJS %>"
         });
     // Stringify all values so we can feed into Webpack DefinePlugin
     var stringified = {
