8f14f850317e252174ae1f469476a6e0d7c4b155
longhao <longtask@gmail.com>
2017-08-12 20:46:08 +0800

    socket.io test

--- src/test/server/socket.io.js
+++ src/test/server/socket.io.js
@@ -3,225 +3,22 @@
 var assert = require('assert');
 var async = require('async');
 var nconf = require('nconf');
-
 var io = require('socket.io-client');
-
-
-console.log("start init session!");
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 var request = require('request');
 var cookies = request.jar();
-var customerSuccessSocket;
-var customerSocket;
 
 var baseUrl = 'http://' + nconf.get('app:address') + ':' + nconf.get('app:port');
 
-request.get(
-    {
-        url: baseUrl + '/test/cs',
-        jar: cookies
-    },
-    function (err, res, body) {
-        if (err) {
-            console.log('Error: ', err);
-            process.exit(0);
-        }
-        customerSuccessSocket = require('socket.io-client')(baseUrl + '/cs',
-            {
-                forceNew: true, reconnectionAttempts: 5,
-                reconnectionDelay: 2000, timeout: 10000,
-                extraHeaders: {
-                    'Cookie': res.headers['set-cookie']
-                }
-            });
+var csURL = baseUrl + '/cs';
 
-        //customer success listener;
-        customerSuccessSocket.on('connect', function () {
-            console.log("-------------- customerSuccessSocket connect!");
-        });
-        customerSuccessSocket.on('disconnect', function () {
-            console.log("-------------- customerSuccessSocket disconnect!");
-        });
-
-        customerSuccessSocket.on('c.message', function (cid, msg) {
-            console.log("---------c.message from------------");
-            console.log(cid);
-            console.log("---------message------------");
-            console.log(msg);
-        });
-
-        customerSuccessSocket.on('cs.customer.list', function (list) {
-            console.log("---------cs.customer.list------------");
-            console.log(list);
-        });
-
-        customerSuccessSocket.on('cs.customer.one', function (data) {
-            console.log("---------cs.customer.one------------");
-            console.log(data);
-        });
-
-        customerSuccessSocket.on('cs.dispatch', function (from, cid, name, key) {
-            console.log("---------cs.dispatch------------");
-            console.log(from);
-            console.log(cid);
-            console.log(name);
-            console.log(key);
-        });
-
-        customerSuccessSocket.on('cs.another.login', function () {
-            console.log("---------cs.another.login------------");
-        });
-
-        customerSuccessSocket.on('disconnect', function (cid) {
-            console.log("---------disconnect------------");
-            console.log(cid);
-        });
-
-        var csid = '1000';
-        var cid = 'c1898';
-
-        async.waterfall([
-            function (next) {
-                setTimeout(function () {
-                    next();
-                }, 1000);
-            },
-            function (next) {
-                customerSuccessSocket.emit("cs.customer.list",
-                    [{"cid": cid, "name": "longhao", "md5": ""}]);
-                next();
-            },
-            function (next) {
-                customerSuccessSocket.emit("cs.customer.one",
-                    {"cid": cid, "name": "longhao", "md5": ""});
-                next();
-            },
-            function () {
-                setTimeout(function () {
-                    customerSuccessSocket.emit("cs.disconnect", csid);
-                }, 3000);
-            }
-        ], function (err) {
-            if (err) {
-                winston.error(err);
-                process.exit();
-            }
-        });
-
-    }
-);
-
-request.get(
-    {
-        url: baseUrl + '/test',
-        jar: cookies
-    },
-    function (err, res, body) {
-        if (err) {
-            console.log('Error: ', err);
-            process.exit(0);
-        }
-
-        customerSocket = require('socket.io-client')(baseUrl + '/c',
-            {
-                forceNew: true, reconnectionAttempts: 5,
-                reconnectionDelay: 2000, timeout: 10000,
-                extraHeaders: {
-                    'Cookie': res.headers['set-cookie']
-                }
-            });
-
-        //customer client listener;
-        customerSocket.on('connect', function () {
-            console.log("-------------- customerSocket connect!");
-        });
-
-        customerSocket.on('cs.message', function (csid, msg) {
-            console.log("---------cs.message from------------");
-            console.log(csid);
-            console.log("---------message------------");
-            console.log(msg);
-        });
-
-        customerSocket.on('cs.dispatch', function (to) {
-            console.log("---------cs.dispatch------------");
-            console.log(to);
-        });
-
-        customerSocket.on('disconnect', function () {
-            console.log("-------------- customerSocket cs.disconnect!");
-        });
-
-        customerSocket.on('c.queue.shift', function (csid) {
-            console.log("---------c.queue.shift------------");
-            console.log(csid);
-        });
-
-        customerSocket.on('c.queue.update', function (num) {
-            console.log("---------c.queue.update------------");
-            console.log(num);
-        });
-
-        customerSocket.on('cs.close', function () {
-            console.log("---------cs.close------------");
-        });
-
-        customerSocket.on('disconnect', function () {
-            console.log("---------disconnect------------");
-        });
-
-        customerSocket.on('cs.status', function () {
-            console.log("---------cs.status------------");
-        });
-
-
-        var cid = 'c1898';
-        var name = "longhao";
-
-        async.waterfall([
-            function (next) {
-                setTimeout(function () {
-                    next();
-                }, 500);
-            },
-            function (next) {
-                customerSocket.emit("c.select", cid, name, function (status, data) {
-                    console.log(status);
-                    console.log(data);
-                    next();
-                });
-            },
-            function (next) {
-                customerSocket.emit("c.message", cid, "hello", function (ok) {
-                    console.log(ok);
-                    next();
-                });
-            },
-            function () {
-                setTimeout(function () {
-                    customerSocket.emit("c.disconnect", cid);
-                    process.exit();
-                }, 5000);
-            }
-        ], function (err) {
-            if (err) {
-                winston.error(err);
-                process.exit();
-            }
-        });
-
-    }
-);
-
+var globalCID = 'c1898';
 
 describe('socket.io', function () {
-    var request = require('request');
-    var cookies = request.jar();
     var customerSuccessSocket;
     var customerSocket;
-    var cid = 'c1898';
     var name = "longhao";
     var csid = '1000';
 
@@ -230,7 +27,7 @@ describe('socket.io', function () {
             function (done) {
                 request.get(
                     {
-                        url: baseUrl + '/test/cs',
+                        url: baseUrl + '/s',
                         jar: cookies
                     },
                     function (err, res, body) {
@@ -238,7 +35,7 @@ describe('socket.io', function () {
                             console.log('Error: ', err);
                             process.exit(0);
                         }
-                        customerSuccessSocket = require('socket.io-client')(baseUrl + '/cs',
+                        customerSuccessSocket = require('socket.io-client')(csURL,
                             {
                                 forceNew: true, reconnectionAttempts: 5,
                                 reconnectionDelay: 2000, timeout: 10000,
@@ -247,19 +44,15 @@ describe('socket.io', function () {
                                 }
                             });
 
-                        //customer success listener;
-                        //customerSuccessSocket.on('connect', function () {
-                        //    console.log("-------------- customerSuccessSocket connect!");
-                        //});
+                        customerSuccessSocket.on('connect', function () {
+                           console.log("-------------- customerSuccessSocket connect!");
+                        });
                         customerSuccessSocket.on('disconnect', function () {
                             console.log("-------------- customerSuccessSocket disconnect!");
                         });
 
                         customerSuccessSocket.on('c.message', function (cid, msg) {
-                            console.log("---------c.message from------------");
-                            console.log(cid);
-                            console.log("---------message------------");
-                            console.log(msg);
+                            assert.equal(cid, globalCID);
                         });
 
                         customerSuccessSocket.on('cs.customer.list', function (list) {
@@ -291,7 +84,7 @@ describe('socket.io', function () {
             function (done) {
                 request.get(
                     {
-                        url: baseUrl + '/test',
+                        url: baseUrl + '/s',
                         jar: cookies
                     },
                     function (err, res, body) {
@@ -309,10 +102,9 @@ describe('socket.io', function () {
                                 }
                             });
 
-                        //customer client listener;
-                        //customerSocket.on('connect', function () {
-                        //    console.log("-------------- customerSocket connect!");
-                        //});
+                        customerSocket.on('connect', function () {
+                           console.log("-------------- customerSocket connect!");
+                        });
 
                         customerSocket.on('cs.message', function (csid, msg) {
                             console.log("---------cs.message from------------");
@@ -376,7 +168,7 @@ describe('socket.io', function () {
 
     it('should select one customer success!', function () {
         console.log("------------");
-        customerSocket.emit("c.select", cid, name, function (ok, data) {
+        customerSocket.emit("c.select", globalCID, name, function (ok, data) {
             console.log(ok);
             console.log(data);
             assert.equal(ok, 2);
@@ -384,7 +176,7 @@ describe('socket.io', function () {
     });
 
     it('should customer emit message!', function () {
-        customerSocket.emit("c.message", cid, "hello", function (ok) {
+        customerSocket.emit("c.message", globalCID, "hello", function (ok) {
             assert.equal(ok, true);
         });
     });
@@ -396,7 +188,7 @@ describe('socket.io', function () {
     });
 
     it('should customer disconnect!', function () {
-        customerSocket.emit("disconnect", cid, function (ok) {
+        customerSocket.emit("disconnect", globalCID, function (ok) {
             assert.equal(ok, true);
         });
     });
@@ -408,21 +200,6 @@ describe('socket.io', function () {
     });
 
 });
-
-
-//customerSocket.request.headers.cookie = res.header['set-cookie'];
-//customerSocket.on('connect', function () {
-//    console.log("-------------- socket.io connect!");
-//});
-//var cid = 'c1898';
-//var name = "longhao";
-//customerSocket.emit("c.select", cid, name, function (ok, data) {
-//    console.log(ok);
-//    console.log(data);
-//    customerSocket.emit("c.message", cid, "hello", function (ok) {
-//        console.log(ok);
-//    });
-//});
 
 //process.exit(0);
 
