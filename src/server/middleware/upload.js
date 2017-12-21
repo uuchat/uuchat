9e0c2634fb715ed77e3f7f066aeeaf121a61aa6d
longhao <longtask@gmail.com>
2017-12-21 10:30:50 +0800

    upload refactor

--- src/server/middleware/upload.js
+++ src/server/middleware/upload.js
@@ -2,182 +2,89 @@
 
 var winston = require('winston');
 var _ = require('lodash');
-var path = require('path');
-var fs = require('fs');
 var async = require('async');
 var nconf = require('nconf');
 var multer = require('multer');
-var sharp = require('sharp');
-var crypto = require('crypto');
+var UploadStorage = require('./uploadStorage');
 var logger = require('../logger');
-var utils = require('../utils');
 var customerSession = require('../database/customerSession');
 
-
 module.exports = function (middleware) {
-    var storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-            var path = getSavePath(nconf.get('images:savePath'));
-            cb(null, path);
-        },
-        filename: function (req, file, cb) {
-            winston.info(file);
-            cb(null, getFileName(file));
-        }
-    });
-
-    var upload = multer({
-        storage: storage,
+    var imageUpload = multer({
+        storage: UploadStorage.imageStorage(),
         limits: {fileSize: nconf.get('images:fileSize')},
-        fileFilter: function (req, file, next) {
-
-            var fileTypes = /jpeg|jpg|png|gif/;
-            var mimeType = fileTypes.test(file.mimetype);
-            var extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
-
-            if (mimeType && extName) return next(null, true);
-
-            var err = new Error("only supports file type:" + fileTypes);
-            err.code = 'FORBID_FILE_TYPE';
-            next(err);
-        }
+        fileFilter: UploadStorage.imageFilter
     });
 
     middleware.uploadImage = function (req, res, next) {
         var condition = {cid: req.params.cid};
         var who = req.query.f;
-        async.waterfall([
-            function(next){
-                customerSession.findOne(condition, next);
 
+        async.waterfall([
+            function (next) {
+                customerSession.findOne(condition, next);
             },
             function (customer, next) {
                 checkMonthFileSize(customer, req, next);
             },
-            function(next){
-                upload.single('image')(req, res, function (err) {
-                    if (err) {
-                        logger.error(err);
-                        return next(new Error('upload-file-has-error'));
-                    }
-                    var fileName = req.file.filename;
-                    if (_.isUndefined(fileName)) {
-                        return next(new Error('upload-file-name-undefined'));
-                    }
-                    var originalname = req.file.originalname;
+            function (next) {
+                imageUpload.single('image')(req, res, function (err) {
+                    if (err) return next(new Error('upload-file-has-error'));
 
-                    var lastUploadSize = req.lastUploadSize;
-                    req.monthlyUploadSize = getYearMonth() + ',' + (parseInt(req.file.size) + parseInt(lastUploadSize));
-
-                    next(null, fileName, originalname);
-                });
-            },
-            function(fileName, originalname, next){
-                crypto.pseudoRandomBytes(16, function (err, raw) {
-                    if (err) {
-                        logger.error(err);
-                        return next(new Error('crypto-name-has-error'));
-                    }
-
-                    var original = path.join(getSavePath(nconf.get('images:savePath')), fileName);
-                    var dest = path.join(getSavePath(nconf.get('images:savePath')),
-                        raw.toString('hex') + path.extname(originalname).toLowerCase());
-
-                    if(!_.isUndefined(who)) { // only update customer
+                    if (!_.isUndefined(who)) { // only update customer
+                        req.monthlyUploadSize = getYearMonth() + ',' + (parseInt(req.file.size) + parseInt(req.lastUploadSize));
                         updateMonthSize(req);
                     }
 
-                    next(null, original, dest);
-                });
-            },
-            function(original, dest, next){
-                var sizeOf = require('image-size');
-                var dimensions = sizeOf(original);
-                var width = dimensions.width;
-                var height = dimensions.height;
-                var resizeWidth = nconf.get('images:width'), resizeHeight = nconf.get('images:height');
-                if (width >= height) {
-                    resizeHeight = resizeWidth * height / width;
-                } else {
-                    resizeWidth = resizeHeight * width / height;
-                }
-
-                winston.info(original);
-
-                sharp(original)
-                    .resize(Math.ceil(resizeWidth), Math.ceil(resizeHeight))
-                    .toFile(dest, function (err, inf) {
-                            if (err) {
-                                logger.error(err);
-                                return next(new Error('resize-file-has-error'));
-                            }
-                            next(null, {code: 200, msg: {original: original, resized: dest, w: resizeWidth, h: resizeHeight}});
+                    next(null, {
+                        code: 200,
+                        msg: {
+                            original: req.file.path,
+                            resized: req.file.resizePath,
+                            w: req.file.resizeWidth,
+                            h: req.file.resizeHeight
                         }
-                    );
+                    });
+                });
             }
         ], function (err, result) {
-            if (err) {
-                switch (err.message) {
-                    case 'customer-not-found':
-                        winston.error(err.message);
-                        res.json({code: 5001, msg: err.message});
-                        break;
-                    case 'exceed-monthly-max-size':
-                        winston.error(err.message);
-                        res.json({code: 5002, msg: err.message});
-                        break;
-                    case 'upload-file-has-error':
-                        winston.error(err.message);
-                        res.json({code: 5003, msg: err.message});
-                        break;
-                    case 'crypto-name-has-error':
-                        winston.error(err.message);
-                        res.json({code: 5004, msg: err.message});
-                        break;
-                    case 'resize-file-has-error':
-                        winston.error(err.message);
-                        res.json({code: 5005, msg: err.message});
-                        break;
-                    case 'upload-file-name-undefined':
-                        winston.error(err.message);
-                        res.json({code: 5006, msg: err.message});
-                        break;
-                    default:
-                        winston.error(err);
-                        res.json({code: 5010, msg: err.message});
-                        break;
-                }
+            if (!err) return res.json(result);
+
+            logger.error(err);
+
+            switch (err.message) {
+                case 'customer-not-found':
+                    res.json({code: 5001, msg: err.message});
+                    break;
+                case 'exceed-monthly-max-size':
+                    res.json({code: 5002, msg: err.message});
+                    break;
+                case 'upload-file-has-error':
+                    res.json({code: 5003, msg: err.message});
+                    break;
+                case 'crypto-name-has-error':
+                    logger.error(err);
+                    break;
+                case 'resize-file-has-error':
+                    res.json({code: 5005, msg: err.message});
+                    break;
+                case 'upload-file-name-undefined':
+                    res.json({code: 5006, msg: err.message});
+                    break;
+                default:
+                    res.json({code: 5010, msg: err.message});
+                    break;
             }
-            res.json(result);
         });
     };
 
-    middleware.uploadAvatar = function(req, res, next) {
-        upload.single('avatars')(req, res, function (err) {
-            var file = req.file;
-            if (err) {
-                logger.error(err);
-                return next(new Error('upload-file-has-error'));
-            }
-            var fileName = req.file.filename;
-            var original = path.join(getSavePath(nconf.get('images:savePath')), fileName);
+    var avatarUpload = multer({
+        storage: UploadStorage.avatarStorage(),
+        limits: {fileSize: nconf.get('images:fileSize')},
+        fileFilter: UploadStorage.imageFilter
+    });
 
-            var fileTypes = path.extname(file.originalname).toLowerCase();
-            var dest = path.join(getSavePath(nconf.get('avatars:savePath')), req.params.csid + fileTypes);
-
-            sharp(original)
-                .resize(nconf.get('avatars:width'), nconf.get('avatars:height'))
-                .toFile(dest, function (err, inf) {
-                        if (err) return next(err);
-                        file.path = dest;
-                        //delete original file;
-                        fs.unlinkSync(original);
-                        next();
-                    }
-                );
-
-        });
-    };
+    middleware.uploadAvatar = avatarUpload.single('avatars');
 
     function checkMonthFileSize(customer, req, next) {
         if (!customer) return next(new Error('customer-not-found'));
@@ -193,8 +100,8 @@ module.exports = function (middleware) {
             }
         }
 
-        if (monthlyUploadSize > nconf.get("images:monthlyMaxSize")) {
-           return next(new Error('exceed-monthly-max-size'));
+        if (monthlyUploadSize - nconf.get("images:monthlyMaxSize") > 0) {
+            return next(new Error('exceed-monthly-max-size'));
         }
 
         req.lastUploadSize = monthlyUploadSize;
@@ -207,46 +114,17 @@ module.exports = function (middleware) {
         var monthlyUploadSize = req.monthlyUploadSize;
         var upload = {upload: monthlyUploadSize};
         var condition = {uuid: req.uploadCacheUUID};
-        customerSession.update(upload, condition, function () {
 
+        setImmediate(function () {
+            customerSession.update(upload, condition, function (err, data) {
+                if (err) logger.error(err);
+            });
         });
     }
 
     function getYearMonth() {
-        //https://stackoverflow.com/questions/3066586/get-string-in-yyyymmdd-format-from-js-date-object
-        Date.prototype.YYYYMM = function() {
-            var mm = this.getMonth() + 1; // getMonth() is zero-based
-
-            return [this.getFullYear(),
-                (mm>9 ? '' : '0') + mm
-            ].join('');
-        };
-        return new Date().YYYYMM();
+        var now = new Date();
+        var mm = now.getMonth() + 1;
+        return [now.getFullYear(), (mm > 9 ? '' : '0') + mm].join('');
     }
-
-    function getFileName(file) {
-        var arr = file.originalname.split('.');
-        var suffix = arr.pop();
-        var name = utils.md5(arr.join('.') + '-' + Date.now());
-        winston.info(name + '.' + suffix);
-        return name + '.' + suffix;
-    }
-
-    function getSavePath(basePath){
-        var date = new Date();
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-        var monthDir = path.join(basePath, month + '');
-        var dayDir = path.join(monthDir, day + '');
-        mkdir(monthDir);
-        mkdir(dayDir);
-        return dayDir;
-    }
-
-    function mkdir(dir) {
-        if (!fs.existsSync(dir)){
-            fs.mkdir(dir);
-        }
-    }
-
 };
